98377c2d9ad78e826ab0c4ecf5d4cc2497093da3
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-01-31 16:28:45 +0300

    Added screenshots

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   //MARK: - Properties
   var locationManager: CLLocationManager?
   var isFirstDetected = false
-  let uuids: [String: String] = ["First":"5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", "Second":"2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6", "Third":"92AB49BE-4127-42F4-B532-90fAF1E26491"]
   var circleView: UIView!
 
   //MARK: - Outlets
@@ -51,34 +50,48 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   }
 
   func startScanning() {
-    for uuid in uuids {
-      let beaconRegion = CLBeaconRegion(proximityUUID: UUID(uuidString: uuid.value)!, major: 123, minor: 456, identifier: uuid.key)
-    locationManager?.startMonitoring(for: beaconRegion)
-    locationManager?.startRangingBeacons(in: beaconRegion)
+    addBeacon(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", major: 123, minor: 456, identifier: "First")
+    addBeacon(uuidString: "E2C56DB5-DFFB-48D2B060D0F5A71096E0", major: 123, minor: 456, identifier: "Second")
+    addBeacon(uuidString: "74278BDA-B644-4520-8F0C-720EAF059935", major: 123, minor: 456, identifier: "Third")
+  }
+
+  func addBeacon(uuidString: String, major: CLBeaconMajorValue, minor: CLBeaconMinorValue, identifier: String) {
+    if let uuid = UUID(uuidString: uuidString) {
+      let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: identifier)
+      locationManager?.startMonitoring(for: beaconRegion)
+      locationManager?.startRangingBeacons(in: beaconRegion)
     }
   }
 
   func update(distance: CLProximity, with name: String) {
 
-    UIView.animate(withDuration: 1) {
+    UIView.animate(withDuration: 0.5) {
       self.beaconNameLabel.text = "\(name)"
       switch distance {
       case .immediate:
         self.view.backgroundColor = .red
         self.distanceReading.text = "IMMEDIATE"
-        self.circleView.transform = CGAffineTransform(scaleX: 1, y: 1)
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+          self.circleView.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
       case .near:
         self.view.backgroundColor = .orange
         self.distanceReading.text = "NEAR"
-        self.circleView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+          self.circleView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        }
       case .far:
         self.view.backgroundColor = .blue
         self.distanceReading.text = "FAR"
-        self.circleView.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+          self.circleView.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
+        }
       default:
         self.view.backgroundColor = .gray
         self.distanceReading.text = "UNKNOWN"
-        self.circleView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+          self.circleView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        }
       }
     }
   }
@@ -95,9 +108,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   func firstBeaconDetection() {
     if !isFirstDetected {
       let alert = UIAlertController(title: "Beacon Detected firs time!", message: nil, preferredStyle: .alert)
-      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [self] _ in
-        isFirstDetected = true
-      }))
+      alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+      isFirstDetected = true
       present(alert, animated: true)
     }
   }
